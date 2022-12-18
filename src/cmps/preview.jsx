5c4aedf8cd8e927bb387e6300afad11c5d5d304e
carmel yona <carmelyona26@gmail.com>
2022-12-18 12:01:34 +0200

    css

--- src/cmps/preview.jsx
+++ src/cmps/preview.jsx
@@ -2,7 +2,7 @@
 
 export const Preview = (props) => {
     const { item, setItem, loadFunc, type } = props
-    return <section className="user-preview flex ">
+    return <section className="preview flex ">
         <div onClick={() => loadFunc(item.id)} >{item.name || item.title}</div>
         {type === 'users' && <button onClick={() => setItem(item)}>Read More..</button>}
     </section>
--- src/pages/todo-page.jsx
+++ src/pages/todo-page.jsx
@@ -1,23 +1,39 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { useEffect } from "react"
 import { List } from "../cmps/list"
 import { Modal } from "../cmps/modal"
 import { UserTodos } from "../cmps/user-todos"
 import { TodosService } from "../services/todos.service"
 import { userService } from "../services/user.service"
+import { Button } from 'react-bootstrap';
+import lottie from 'lottie-web'
+import { Header } from "../cmps/header"
+import { NavBar } from "../cmps/nav-bar"
 
 
 export const TodoPage = () => {
     const [users, setUsers] = useState([])
     const [user, setUser] = useState(null)
     const [todos, setTodos] = useState(null)
+    const container = useRef(null)
 
     useEffect(() => {
         loadUsers()
+
+        lottie.loadAnimation({
+            container: container.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../assets/animations/loading.json'),
+        })
     }, [])
 
     const loadUsers = async () => {
-        setUsers(await userService.getUsers())
+        setTimeout(async () => {
+            setUsers(await userService.getUsers())
+
+        }, 2300)
     }
 
     const loadTodos = async (userId) => {
@@ -30,13 +46,17 @@ export const TodoPage = () => {
         loadUsers()
     }
 
-    if (!users?.length) return <div>Loading..</div>
-    return <section className="todo-page">
-        <div className="page-body flex">
-            <List loadFunc={loadTodos} items={users} setItem={setUser} type='users' />
-            {todos ? <UserTodos todos={todos} setTodos={setTodos} /> : <div><h1>Todos List:</h1></div>}
+    if (!users?.length) return <div className="loading-animation flex" ref={container}></div>
+    return (<section className="todo-page flex">
+        <div className="page-body flex column">
+            <Header />
+            <div className="page-body-container flex">
+                <NavBar />
+                <List loadFunc={loadTodos} items={users} setItem={setUser} type='users' />
+                {todos ? <UserTodos todos={todos} setTodos={setTodos} /> : <div><h1>Todos List:</h1></div>}
+            </div>
         </div>
 
         {user && <Modal item={user} setItem={setUser} type='user' updateFunc={updateUser} />}
-    </section>
+    </section>)
 }
